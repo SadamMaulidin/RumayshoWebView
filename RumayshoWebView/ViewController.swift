62902c73779c98a4af269fd95defe7089438f800
SaeMaulidin <sadammaulidin@gmail.com>
2021-03-10 20:42:13 +0700

    Initial Commit

---
+++ RumayshoWebView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RumayshoWebView
+//
+//  Created by Sadam Maulidin on 10/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
